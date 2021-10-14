e59edfb1b9d3a0b70970de89398acffff964e182
Chatterjee <aritra.a.chatterjee@accenture.com>
2021-10-14 09:55:37 +0530

    added the initial app.js file

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import  { useState } from 'react';
+
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [outputtext, setoutputtext] = useState("Type here to change the text.")
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>jhjhjh</Text>
+      <Button title="Change Text"></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
